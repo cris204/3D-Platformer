9da6b7f75a21a23019a8d37b94228cccc3549986
cristian <crist.1707@hotmail.com>
2017-11-26 06:15:03 -0500

    bug salto

--- Assets/Proyecto Completo/Scripts/PlayerController.cs
+++ Assets/Proyecto Completo/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour {
 	private Rigidbody rb;
 	[SerializeField]
 	private float speed;
+
 	[SerializeField]
 	private float jump;
 	Vector3 rotar;
@@ -17,6 +18,9 @@ public class PlayerController : MonoBehaviour {
 	private float deltaJump;
 	private float h;
 	private float v;
+	private bool run;
+	private bool canJump;
+
 	// Use this for initialization
 	void Start () {
 		rb=GetComponent<Rigidbody>();	
@@ -36,24 +40,45 @@ public class PlayerController : MonoBehaviour {
 	void GetInput(){
 		h=Input.GetAxis("Horizontal");
 		v=Input.GetAxis("Vertical");
+		run=Input.GetButton("Run");
 	}
 
 	void Jump(){
 
-		if(Input.GetButton("Jump")){
+		if(Input.GetButton("Jump")&&canJump){
 			deltaJump=jump*Time.deltaTime;
 			rb.velocity=new Vector3(rb.velocity.x,deltaJump,rb.velocity.z);
 		}
 	}
 
 	void Move(){
-		if(h!=0 || v!=0){
+		if(run){
+			deltaSpeed=speed*2*Time.deltaTime;
+		}else{
 			deltaSpeed=speed*Time.deltaTime;
-			rb.velocity=(v*transform.forward + h*transform.right+transform.up*rb.velocity.y)*deltaSpeed;
+		}
+
+		if(h!=0 || v!=0){
+			
+			rb.velocity=(((v*transform.forward) +( h*transform.right))*deltaSpeed)+(transform.up*rb.velocity.y);
+			
 		}
 	}
 
 	void Girar(){
 		transform.rotation = Quaternion.Euler (0f, Input.mousePosition.x , 0f);
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.tag=="Terrain"){
+			canJump=true;
+		}
+	}
+	void OnCollisionExit(Collision other)
+	{
+		if(other.gameObject.tag=="Terrain"){
+			canJump=false;
+		}
 	}
 }
